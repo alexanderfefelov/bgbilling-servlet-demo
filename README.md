8db3512ea389da3d8a6b53277be15cb85e318296
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-12-29 15:24:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# bgbilling-servlet-demo
+
+```
+git clone https://github.com/alexanderfefelov/bgbilling-servlet-demo.git
+cd bgbilling-servlet-demo
+mvn package
+```
+
+```
+custom.servlet.keys=DemoServlet
+custom.servlet.DemoServlet.class=com.github.alexanderfefelov.bgbilling.servlet.demo.DemoServlet
+custom.servlet.DemoServlet.mapping=/DemoServlet
+```
---
+++ src/main/java/com/github/alexanderfefelov/bgbilling/servlet/demo/DemoServlet.java
@@ -0,0 +1,52 @@
+package com.github.alexanderfefelov.bgbilling.servlet.demo;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        logger.trace("init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("doGet");
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello, World!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("doPost");
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("doPut");
+        super.doPut(request, response);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("doDelete");
+        super.doDelete(request, response);
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.trace("doHead");
+        super.doHead(request, response);
+    }
+
+    private Logger logger = Logger.getLogger(this.getClass());
+
+}
